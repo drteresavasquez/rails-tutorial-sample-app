cd72c2601ea0e7e8fe53d02389b8199440f131e7
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-12 16:46:31 -0600

    Stopped at 8.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help', as: 'helf' #This allows us to call the path one thing but have the route be something else
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
